94fa77fdeab126cdcd13b16189a6319d4baeab76
code1009 <code1009@naver.com>
2025-06-20 16:47:08 +0900

    1

---
+++ MyApp/UI/MyDialog.xaml.cpp
@@ -0,0 +1,58 @@
+﻿#include "pch.h"
+
+#include "Page1.xaml.h"
+
+#if __has_include("Page1.g.cpp")
+
+#include "Page1.g.cpp"
+
+#endif
+
+#include "NavigationParam.h"
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Controls;
+using namespace Microsoft::UI::Xaml::Navigation;
+using namespace Windows::Foundation;
+
+namespace winrt::MyApp::implementation
+{
+    Page1::Page1()
+    {
+        InitializeComponent();
+    }
+
+    void Page1::OnNavigatedTo(NavigationEventArgs const& eventArgs)
+    {
+        auto param = eventArgs.Parameter().try_as<winrt::MyApp::NavigationParam>();
+        if (param)
+        {
+            _MainPage = param.MainPage();
+        }
+        if (!_MainPage)
+        {
+            throw hresult_error(E_FAIL, L"MainPage is not available.");
+		}
+    }
+
+    void Page1::SuccessMessage_Click(IInspectable const& /*sender*/, RoutedEventArgs const& /*eventArgs*/)
+    {
+        _MainPage.NotifyUser(L"Everything was ok!", InfoBarSeverity::Success);
+    }
+
+    void Page1::ErrorMessage_Click(IInspectable const& /*sender*/, RoutedEventArgs const& /*eventArgs*/)
+    {
+        _MainPage.NotifyUser(L"Something went wrong.", InfoBarSeverity::Error);
+    }
+
+    void Page1::InformationalMessage_Click(IInspectable const& /*sender*/, RoutedEventArgs const& /*eventArgs*/)
+    {
+        _MainPage.NotifyUser(L"This is the informational bar.", InfoBarSeverity::Informational);
+    }
+
+    void Page1::ClearMessage_Click(IInspectable const& /*sender*/, RoutedEventArgs const& /*eventArgs*/)
+    {
+        _MainPage.NotifyUser(L"", InfoBarSeverity::Informational);
+    }
+}
---
+++ MyApp/UI/MyDialog.xaml.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Page1.g.h"
+
+namespace winrt::MyApp::implementation
+{
+    struct Page1 : Page1T<Page1>
+    {
+    private:
+        winrt::MyApp::MainPage _MainPage{ nullptr };
+
+    public:
+        Page1();
+
+    public:
+        void OnNavigatedTo(Microsoft::UI::Xaml::Navigation::NavigationEventArgs const& eventArgs);
+
+    public:
+        void SuccessMessage_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& eventArgs);
+        void ErrorMessage_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& eventArgs);
+        void InformationalMessage_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& eventArgs);
+        void ClearMessage_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& eventArgs);
+    };
+}
+
+namespace winrt::MyApp::factory_implementation
+{
+    struct Page1 : Page1T<Page1, implementation::Page1>
+    {
+    };
+}
